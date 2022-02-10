68d6767c6fd925c0e2d87765e83fa3a3f3ad5457
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2022-02-10 15:12:10 +0100

    feat(model): add model for phone

---
+++ src/Domain/Phone/Exception/InvalidPriceException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Phone\Exception;
+
+use RuntimeException;
+
+class InvalidPriceException extends RuntimeException
+{
+}
--- src/Domain/Phone/Model/Phone.php
+++ src/Domain/Phone/Model/Phone.php
@@ -1,9 +1,78 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Domain\Phone\Model;
 
+use App\Domain\Phone\ValueObject\Price;
+use Ramsey\Uuid\Uuid;
+
 class Phone
 {
+    private string $id;
 
+    private string $builder;
+
+    private string $model;
+
+    private string $color;
+
+    private string $description;
+
+    private Price $price;
+
+    public function __construct(
+        string $builder,
+        string $color,
+        string $description,
+        string $model,
+        Price $price
+    ) {
+        $this->id = (string) Uuid::uuid4();
+        $this->builder = $builder;
+        $this->color = $color;
+        $this->description = $description;
+        $this->model = $model;
+        $this->price = $price;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getBuilder(): string
+    {
+        return $this->builder;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function changeDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price->getPrice();
+    }
+
+    public function getFormattedPrice(): string
+    {
+        return (string) $this->price;
+    }
 }
